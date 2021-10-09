540bd77c3d3b76801d82ca903dced2158407669c
gintooooonic <gintooooonic@gmail.com>
2021-10-09 14:16:54 +0900

    정렬 비교 함수 수정
    Stable Sort 보장

--- src/List/index.js
+++ src/List/index.js
@@ -1,7 +1,7 @@
 import style from "./index.module.scss";
 
 export default function List(props) {
-  const notDoneFirst = (a, b) => (a.done ? !b.done : -1);
+  const notDoneFirst = (a, b) => (a.done ? !b.done : -b.done);
   const jobs = [...props.jobs].sort(notDoneFirst);
   const listItems = jobs.map((job, idx) => <ListItem key={idx} job={job} />);
   return <ul className={style.list}>{listItems}</ul>;
